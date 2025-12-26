525b6c08ac6c5bd7ade31b9f88560f4a6aebb6bd
bouhouchhamza <bouhouchhamza52@gmail.com>
2025-12-26 15:04:40 +0100

    feat(crud): implement projet CRUD

--- Crudes/projet.php
+++ Crudes/projet.php
@@ -8,7 +8,7 @@ function ajouterProjet($titre, $type, $membre_id)
     $db = new Database();
     $pdo = $db->pdo;
 
-    $sql = "INSERT INTO projets (titre, type, membre_id)
+    $sql = "INSERT INTO projet (titre, type, membre_id)
             VALUES (:titre, :type, :membre_id)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
@@ -28,8 +28,8 @@ function afficherProjets()
 
     $sql = "
         SELECT p.id, p.titre, p.type, m.nom
-        FROM projets p
-        JOIN membres m ON p.membre_id = m.id
+        FROM projet p
+        JOIN membre m ON p.membre_id = m.id
     ";
     $stmt = $pdo->query($sql);
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -46,14 +46,14 @@ function supprimerProjet($id)
     $pdo = $db->pdo;
 
     
-    $check = $pdo->prepare("SELECT COUNT(*) FROM activites WHERE projet_id = :id");
+    $check = $pdo->prepare("SELECT COUNT(*) FROM activite WHERE projet_id = :id");
     $check->execute(["id" => $id]);
     if ($check->fetchColumn() > 0) {
         echo "❌ Projet فيه activités، ما يمكنش يتحيد\n";
         return;
     }
 
-    $stmt = $pdo->prepare("DELETE FROM projets WHERE id = :id");
+    $stmt = $pdo->prepare("DELETE FROM projet WHERE id = :id");
     $stmt->execute(["id" => $id]);
 
     echo "🗑️ Projet supprimé\n";
